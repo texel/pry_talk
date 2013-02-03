8d899b38f88683853848bbb01ee432400f8b63ef
Leigh Caplan <lcaplan@onehub.com>
2013-02-03 14:44:36 -0800

    Person class

---
+++ person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(name, height, weight = 0)
+    @name   = name
+    @height = height
+    @weight = weight
+  end
+
+  def description
+    d = "I am #{@name}. I am #{@height} feet tall"
+    d << " and I weigh #{@weight} pounds" if @weight > 0
+    d << "."
+  end
+end
